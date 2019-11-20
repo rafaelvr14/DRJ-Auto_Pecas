b0938ce0df2bd9fb7391acf2227f719534d2353c
rafaelvr14 <rafael.fernandes@estudante.ufla.br>
2019-11-20 02:33:26 -0300

    concluindo a base de algumas páginas

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,3 +1,11 @@
-function mudaOculos(foto){
-            document.getElementById("icone").src=foto;
+function validacao(){
+    var password = document.getElementById("password")
+    , confirm_password = document.getElementById("confirm_password");
+    if(password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Senhas diferentes!");
+    } else {
+        confirm_password.setCustomValidity("");
         }
+    password.onchange = validatePassword;
+    confirm_password.onkeyup = validatePassword;
+}
