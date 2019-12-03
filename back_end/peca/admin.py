4d9a6026c719ac2fad813dd5aa73dd4d16de943b
jpandolpho <joaopedrodealmeidaandolpho@gmail.com>
2019-12-03 12:41:59 -0300

    Adicionando novas tabelas ao banco
    Adicionando tabelas de Carrinho, Desconto e tabelas intermediarias ao
    banco de dados

--- back_end/peca/admin.py
+++ back_end/peca/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Peca
+from .models import Peca, Desconto, DescontoPeca, Carrinho
 
 admin.site.register(Peca)
+admin.site.register(Desconto)
+admin.site.register(DescontoPeca)
+admin.site.register(Carrinho)
--- back_end/peca/models.py
+++ back_end/peca/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Peca(models.Model):
     nome = models.CharField(max_length=75)
-    codigo = models.PositiveSmallIntegerField()
     preco = models.DecimalField(max_digits=6,decimal_places=2)
+    estoque = models.PositiveIntegerField()
+
+class Desconto(models.Model):
+    valor = models.DecimalField(max_digits=4,decimal_places=2)
+
+class DescontoPeca(models.Model):
+    peca = models.ForeignKey('Peca', on_delete=models.CASCADE)
+    desconto = models.ForeignKey('Desconto', on_delete=models.CASCADE)
+
+class PecaCarrinho(models.Model):
+    peca = models.ForeignKey('Peca',on_delete=models.CASCADE)
+    qtde = models.PositiveIntegerField()
+
+class Carrinho(models.Model):
+    pecas = models.ManyToManyField('PecaCarrinho')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10,decimal_places=2)
