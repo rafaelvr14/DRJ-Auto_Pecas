340ae3a15a137ac035d5b3ade35958d0e7cde773
jpandolpho <joaopedrodealmeidaandolpho@gmail.com>
2019-12-04 12:58:06 -0300

    Adicionando novas migracoes do django

--- back_end/peca/models.py
+++ back_end/peca/models.py
@@ -21,4 +21,5 @@ class PecaCarrinho(models.Model):
 class Carrinho(models.Model):
     pecas = models.ManyToManyField('PecaCarrinho')
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    realizado = models.BooleanField(default=False)
     total = models.DecimalField(max_digits=10,decimal_places=2)
