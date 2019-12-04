faecc78323244dc4a903ffe6b46e827daadaec5b
jpandolpho <joaopedrodealmeidaandolpho@gmail.com>
2019-12-04 12:58:36 -0300

    Adicionando arquivos e funcoes para a ligacao
    Adicionando o arquivo /peca/urls.py, que contem os caminhos para as
    chamadas dos html.
    Alterando o arquivo /drjautopecas/urls.py para que ele reconheca os
    caminhos criados em /peca/urls.py.
    Alterando o arquivo peca/views.py, criando as funcoes bases para
    navegacao da aplicacao. Nem todas as paginas ainda sao acessiveis e elas
    nao fazem, efetivamente, nada ainda, sendo assim, as funcoes servem
    apenas para caminhar entre as paginas da aplicacao.

---
+++ back_end/peca/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',index_view, name='index'),
+    path('index/',index_view, name='index'),
+    path('login/',login_view, name='login'),
+    path('verificacao/',verifica_view, name='verificacao'),
+    path('drjautopecas/',logado_view, name='logado'),
+    path('drjadmin/',admin_login_view, name='admlogin'),
+    path('cadastro/',registro_view, name='cadastro'),
+    path('cadfixa/',registro_fixoa_view,name='fixoa'),
+    path('cadfixb',registro_fixob_view,name='fixob'),
+    path('carrinho/',carrinho_view,name='carrinho'),
+    path('finalizar/',finalizar_view,name='finalizar'),
+    path('consulta-avulso/',consulta_avulso_view,name='consulta-avulso'),
+    path('consulta-fixo/',consulta_fixo_view,name='consulta-fixo'),
+    path('repositorio/',repositorio_view,name='repositorio'),
+    path('descontos/',desconto_view,name='descontos'),
+    path('adicionar-peca/',adiciona_peca_view,name='addpeca'),
+    path('adiciona-desconto/',adiciona_desconto_view,name='adddscnt')
+]
--- back_end/peca/views.py
+++ back_end/peca/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index_view(request):
+    return render(request, "index.html")
+
+def login_view(request):
+    return render(request, "_paginas/logar.html")
+
+def verifica_view(request):
+    return render(request, "_paginas/verificadorCliente.html")
+
+def logado_view(request):
+    return render(request, "_paginas/logado.html")
+
+def admin_login_view(request):
+    return render(request, "_paginas/Admin_login.html")
+
+def registro_view(request):
+    return render(request, "_paginas/registro.html")
+
+def registro_fixoa_view(request):
+    return render(request,"_paginas/registro_clienteFixo.html")
+
+def registro_fixob_view(request):
+    return render(request,"_paginas/registro_clienteFixo2.html")
+
+def carrinho_view(request):
+    return render(request,"_paginas/finalizar.html")
+
+def finalizar_view(request):
+    return render(request,"_paginas/finalizarCompra.html")
+
+def consulta_avulso_view(request):
+    return render(request, "_paginas/consultarClienteAvulso.html")
+
+def consulta_fixo_view(request):
+    return render(request, "_paginas/consultarClienteFixo.html")
+
+def repositorio_view(request):
+    return render(request, "_paginas/repositorio.html")
+
+def desconto_view(request):
+    return render(request, "_paginas/descontos.html")
+
+def adiciona_peca_view(request):
+    return render(request,"_paginas/adicionarPeca.html")
+
+def adiciona_desconto_view(request):
+    return render(request,"_paginas/adicionarDesconto.html")
